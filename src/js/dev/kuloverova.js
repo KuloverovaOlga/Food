7e0bc8c86f825b24461ac5421cba7777b044dcac
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-02-08 14:01:09 +0400

    add catering page

--- src/js/dev/kuloverova.js
+++ src/js/dev/kuloverova.js
@@ -2,9 +2,12 @@ import popup from '../utils/popup';
 import form from '../utils/form';
 import CircleType from 'circletype';
 import 'inputmask';
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
 
 export const modules = {};
 window.$ = window.jQuery = require('jquery');
+import { rem } from '../utils/constants';
 
 document.addEventListener('DOMContentLoaded', () => {
   try {
@@ -22,7 +25,21 @@ document.addEventListener('DOMContentLoaded', () => {
   try {
     guaranteeCircles();
   } catch {}
-
+  try {
+    otherServicesCircles();
+  } catch {}
+  try {
+    mainTabs();
+  } catch {}
+  try {
+    ourWorksSwiper();
+  } catch {}
+  try {
+    otherServicesSwiper();
+  } catch {}
+  try {
+    reviewsSwiper();
+  } catch {}
 });
 
 function burgerMenu() {
@@ -75,4 +92,87 @@ function guaranteeCircles() {
   });
 }
 
+function otherServicesCircles() {
+  const circles = document.querySelectorAll('.other-services__circle');
 
+  circles.forEach((circle) => {
+    const circleType = new CircleType(circle);
+    circle.style.transform = 'rotate(105deg)';
+  });
+}
+
+function mainTabs() {
+  const buttons = document.querySelectorAll('.main-tabs__tabs-btn ');
+  const tabs = document.querySelectorAll('.main-tabs__tabs-tab');
+
+  buttons.forEach((button, i) => {
+    button.addEventListener('click', () => {
+      buttons.forEach((item) => item.classList.remove('active'));
+      tabs.forEach((tabs) => tabs.classList.remove('active'));
+      button.classList.add('active');
+      tabs[i].classList.add('active');
+    });
+  });
+}
+
+function ourWorksSwiper() {
+  const swiperOurWorks = new Swiper('.our-works__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    speed: 300,
+    // allowTouchMove: false,
+    navigation: {
+      nextEl: '.our-works__swiper-btn--next',
+      prevEl: '.our-works__swiper-btn--prev'
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: rem(3.2),
+      }
+    }
+  });
+}
+
+function otherServicesSwiper() {
+  const swiperOurWorks = new Swiper('.other-services-swiper__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    speed: 300,
+    // allowTouchMove: false,
+    navigation: {
+      nextEl: '.other-services-swiper__btn--next',
+      prevEl: '.other-services-swiper__btn--prev'
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: rem(3.2),
+      }
+    }
+  });
+}
+
+function reviewsSwiper() {
+  const swiperOurWorks = new Swiper('.reviews-swiper__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    speed: 300,
+    autoHeight: true,
+    // allowTouchMove: false,
+    navigation: {
+      nextEl: '.reviews-swiper__btn--next',
+      prevEl: '.reviews-swiper__btn--prev'
+    },
+
+    
+  });
+}
--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,3 +1,13 @@
+export 	const rem = function (rem) {
+  if (window.innerWidth > 768) {
+    return 0.005208335 * window.innerWidth * rem;
+  } else {
+    // где 375 это ширина мобильной версии макета
+    return (100 / 375) * (0.05 * window.innerWidth) * rem;
+  }
+  
+}
+
 export let bodyLockStatus = true;
 
 export let bodyUnlock = (delay = 500) => {
